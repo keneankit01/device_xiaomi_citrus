1ad7b0887a798f6d642991bd4d3a2f54469a2b01
ANKIT KENE <47526753+keneankit01@users.noreply.github.com>
2021-03-23 23:47:03 +0530

    Ancify-part1

--- ancient_citrus.mk
+++ ancient_citrus.mk
@@ -9,13 +9,11 @@ $(call inherit-product, device/xiaomi/citrus/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
-#build Official-ly
-DERP_BUILDTYPE := Official
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_citrus
+PRODUCT_NAME := ancient_citrus
 PRODUCT_DEVICE := citrus
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO M3
